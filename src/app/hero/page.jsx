c27c8eda506d09ede55bedd4a24cec976bf9c2d2
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-04-30 18:38:14 +0600

    first commit

---
+++ src/app/hero/page.jsx
@@ -0,0 +1,20 @@
+import { Button } from '@heroui/react';
+import React from 'react';
+
+const HeroPage = () => {
+    return (
+        <div>
+            <h1>Hero Page</h1>
+            <Button>
+                Hero Button
+            </Button>
+
+            <Button variant="outline">
+                Hero Outline Button
+            </Button>
+
+        </div>
+    );
+};
+
+export default HeroPage;
---
+++ src/app/tasks/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TasksPage = () => {
+    return (
+        <div>
+            <h2>Tasks: 0</h2>
+        </div>
+    );
+};
+
+export default TasksPage;
